2103728623df81845a855dc3c8329ca52afc04f6
david <davidinsuasty@gmail.com>
2020-07-14 21:50:43 -0500

    se agrega ejemplo de reporte

---
+++ script_prueba_reporte.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 21:18:14 2020
+
+@author: hinsuasti
+"""
+
+from leila import datos_gov, reporte
+
+
+# Abrir la base de datos original con los metadatos (asset inventory)
+# del Portal de Datos Abiertos
+inventario = datos_gov.tabla_inventario()
+
+# Buscar bases cuyo nombre incluye temas de SECOP
+columnas_valor = {"nombre":["SECOP"]}
+tabla_filtrada = datos_gov.filtrar_tabla(columnas_valor)
+
+# Cargar una base de datos de interes
+#cargando la prmera base de datos que aparece en la tabla filtrada por
+#la palabra "SECOP"
+tabla_id = tabla_filtrada.iloc[10].numero_api
+datos = datos_gov.cargar_base(tabla_id)
+
+# Generar reporte con el modulo reporte
+reporte.generar_reporte(df = datos)
